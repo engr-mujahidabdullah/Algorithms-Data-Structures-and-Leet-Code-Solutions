d941f4f39bb3138c8731392606583b4cf14c47b9
engr-mujahidabdullah <61884536+engr-mujahidabdullah@users.noreply.github.com>
2023-04-06 00:55:19 +0500

    solutions froms leet code also added

--- Algors.py
+++ Algors.py
@@ -233,9 +233,17 @@ class Algors:
                     pairs = pairs + 1
         return pairs
 
-
-
-
+    """
+    Build Array from Permutation
+    Given a zero-based permutation nums (0-indexed), build an array ans of the same length
+    where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it. A zero-based
+    permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+    """
+    def buildArray(self, nums: list[int]) -> list[int]:
+        out = []
+        for i in range(len(nums)):
+            out.append(nums[nums[i]])
+        return out
 
 if __name__ == "__main__":
     test = Algors()
